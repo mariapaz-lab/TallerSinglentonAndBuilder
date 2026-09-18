b6bb4c5f0e5ae81b00219e71d2879e785a5bff8d
Nikol Gil <nikol.gilc@uqvirtual.edu.co>
2026-09-17 20:34:10 -0500

    Implementacion de ConsecutiveFactura

--- src/main/java/App/ConsecutiveFactura.java
+++ src/main/java/App/ConsecutiveFactura.java
@@ -1,4 +1,23 @@
 package App;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ConsecutiveFactura {
+    private final AtomicInteger unico = new AtomicInteger(0);
+
+    private ConsecutiveFactura() {} //constructor
+
+    private static class Holder { //holder
+
+        private static final ConsecutiveFactura INSTANCIA =
+                new ConsecutiveFactura();
+    }
+
+    public static ConsecutiveFactura getInstancia() {
+        return Holder.INSTANCIA;
+    }
+
+    public int siguiente() {
+        return unico.incrementAndGet();
+    }
 }
